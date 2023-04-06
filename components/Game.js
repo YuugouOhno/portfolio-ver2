5582458e1dc7f2ee2ade7d73cae6f5ce2ff48049
YuugouOhno <yuugou.ohno@gmail.com>
2023-04-06 09:09:58 +0900

    最後のページの一言を追加

--- components/Game.js
+++ components/Game.js
@@ -3,11 +3,8 @@ const Game = () => {
         <div id="game" className="h-screen w-screen flex flex-col justify-center items-center">
             <div className="mt-56">
                 <h1 className="text-8xl font-bold text-center my-4">
-                    Yuugou Ohno
+                    Bye
                 </h1>
-                <h2 className="text-5xl font-bold text-center">
-                    web engineer
-                </h2>
             </div>
         </div>
     )
