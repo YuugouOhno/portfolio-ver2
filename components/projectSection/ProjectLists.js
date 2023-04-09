1f4b39ab1a2046ce481f9bfd3d0be650c42ab3bc
YuugouOhno <yuugou.ohno@gmail.com>
2023-04-10 07:59:30 +0900

    プロジェクトにポートフォリオ１を追加

--- components/projectSection/ProjectLists.js
+++ components/projectSection/ProjectLists.js
@@ -22,23 +22,37 @@ const ProjectLists = [
     },
     {
         id: 4,
-        title: '魚',
+        title: 'portfolio-ver1',
         image_path: '/projects/4.png',
         tags: ["JavaScript", "Three.js"],
         url: ""
     },
     {
         id: 5,
-        title: '僕の考えた最強の名刺',
+        title: '魚',
         image_path: '/projects/5.png',
-        tags: ["JavaScript", "Three.js", "AR.js"],
+        tags: ["JavaScript", "Three.js"],
         url: ""
     },
     {
         id: 6,
-        title: 'モテ逃げボタン',
+        title: '僕の考えた最強の名刺',
         image_path: '/projects/6.png',
+        tags: ["JavaScript", "Three.js", "AR.js"],
+        url: ""
+    },
+    {
+        id: 7,
+        title: 'モテ逃げボタン',
+        image_path: '/projects/7.png',
         tags: ["JavaScript"],
+        url: ""
+    },
+    {
+        id: 8,
+        title: 'portfolio-ver2',
+        image_path: '/projects/4.png',
+        tags: ["Next.js"],
         url: ""
     },
 ];
