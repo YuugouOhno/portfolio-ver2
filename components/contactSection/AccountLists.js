fe05a69834915e38f4468c9e9a51fc58c979a75c
YuugouOhno <yuugou.ohno@gmail.com>
2023-04-10 23:52:56 +0900

    拡張子を変更、githubを先頭に移動 #14

--- components/contactSection/AccountLists.js
+++ components/contactSection/AccountLists.js
@@ -1,16 +1,16 @@
 const AccountLists = [
     {
+        src_dark:"/accounts/github-white.png",
+        src_light:"/accounts/github-black.png",
+        title:"github",
+        url:"https://github.com/YuugouOhno"
+    },
+    {
         src_dark:"/accounts/twitter.png",
         src_light:"/accounts/twitter.png",
         title:"twitter",
         url:"https://twitter.com/YuugouOhno"
         
-    },
-    {
-        src_dark:"/accounts/github-white.png",
-        src_light:"/accounts/github-black.png",
-        title:"github",
-        url:"https://github.com/YuugouOhno"
     },
     {
         src_dark:"/accounts/qita.png",
