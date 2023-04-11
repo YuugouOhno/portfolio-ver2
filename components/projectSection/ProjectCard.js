d54c10ace3413c7a5623259729dc3c6d2a52d62c
YuugouOhno <yuugou.ohno@gmail.com>
2023-04-11 10:17:13 +0900

    #7 モーダルっぽい感じになった

--- components/projectSection/ProjectCard.js
+++ components/projectSection/ProjectCard.js
@@ -1,5 +1,5 @@
 import AnimatedComponent from '@/components/animation/AppearanceContainer';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 const ProjectCard = ({ title, children }) => {
     const [isFlipped, setIsFlipped] = useState(false);
@@ -7,7 +7,7 @@ const ProjectCard = ({ title, children }) => {
     const styles = {
         flipCard: {
             width: '24rem',
-            height: '13rem',
+            height: '13.5rem',
             perspective: '1000px',
             transition: 'transform 0.6s',
             textAlign: 'center',
@@ -16,13 +16,13 @@ const ProjectCard = ({ title, children }) => {
             position: 'fixed',
             top: "50%",
             left: "50%",
-            width: '24rem',
-            height: '13rem',
-            transform: "translateX(-50%) translateY(-50%)",
+            width: '80%',
+            height: '70%',
+            transform: "translateX(-50%) translateY(-44%)",
             perspective: '1000px',
             transition: 'transform 0.6s',
             textAlign: 'center',
-            zIndex: "1000"
+            zIndex: "100"
         },
         flipCardInner: {
             width: '100%',
@@ -56,9 +56,18 @@ const ProjectCard = ({ title, children }) => {
             backfaceVisibility: 'hidden',
             transform: 'rotateY(180deg)',
             textAlign: 'center',
-            backgroundColor: 'gray',
+            backgroundColor: 'red',
             borderRadius: "0.375rem",
         },
+        overwrap: {
+            position: "fixed",
+            top: "0",
+            left: "0",
+            width: "100%",
+            height: "100%",
+            zIndex: "10",
+            backgroundColor: 'rgba(255,255,255,0.7)',
+        }
     }
 
     const flipCard = () => {
@@ -68,10 +77,10 @@ const ProjectCard = ({ title, children }) => {
     return (
         <AnimatedComponent>
             <div className="cursor-pointer group flex flex-col justify-center items-center">
-
-                <div style={isFlipped ? styles.flippedCard : styles.flipCard} onClick={flipCard}>
+                {isFlipped ? <div style={styles.overwrap} onClick={flipCard}></div> : ""}
+                <div style={isFlipped ? styles.flippedCard : styles.flipCard}>
                     <div style={isFlipped ? styles.flippedCardInner : styles.flipCardInner} >
-                        <div style={styles.flipCardFront}>
+                        <div style={styles.flipCardFront} onClick={flipCard}>
                             <div className="relative overflow-hidden transition-all hover:scale-105 flex justify-center items-center">
                                 {children[0]}
                             </div>
@@ -81,6 +90,7 @@ const ProjectCard = ({ title, children }) => {
                         </div>
                     </div>
                 </div>
+
                 <h2 className="mt-2 text-xl text-center font-semibold tracking-normal text-brand-primary my-4">
                     <span
                         className="bg-gradient-to-r from-green-200 to-green-100 dark:from-purple-800 dark:to-purple-900
