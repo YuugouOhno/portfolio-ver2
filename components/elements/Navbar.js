a01a706b5b490cf33d9732280e0607b898379b3f
YuugouOhno <yuugou.ohno@gmail.com>
2023-04-14 07:59:06 +0900

    #11 鉤括弧のアニメーション

--- components/elements/Navbar.js
+++ components/elements/Navbar.js
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import DarkModeToggle from '@/components/elements/DarkModeToggle';
 import AddFish from '@/components/elements/AddFish';
 import LanguageToggle from '@/components/elements/LanguageToggle';
+import Brackets from '@/components/elements/Brackets';
 import { FaHamburger } from "react-icons/fa";
 import { useState, useEffect } from 'react';
 
@@ -20,7 +21,7 @@ const Navbar = () => {
                 }
             });
         };
-        
+
         window.addEventListener('scroll', handleScroll);
 
         return () => {
@@ -35,38 +36,39 @@ const Navbar = () => {
                     YuugouOhno
                 </div>
                 <div className="flex md:hidden p-3">
-                    <FaHamburger className="h-6 w-6" onClick={()=>setIsOpen(!isOpen)} />
+                    <FaHamburger className="h-6 w-6" onClick={() => setIsOpen(!isOpen)} />
                     <div className={isOpen ? "fixed top-0 left-0 bold font-medium text-7xl mt-20 w-auto p-4" : 'hidden'}>
-                    <ul className="flex flex-col">
-                        <li className="dark:bg-black dark:bg-opacity-70 bg-white bg-opacity-70 rounded-lg my-1">
-                            <Link href="#home" className={activeSection == 'home' ? 'text-purple-600' : ''}>Home</Link>
-                        </li>
-                        <li className="dark:bg-black dark:bg-opacity-70 bg-white bg-opacity-70 rounded-lg my-1">
-                            <Link href="#about" className={activeSection == 'about' ? 'text-purple-600' : ''}>About</Link>
-                        </li>
-                        <li className="dark:bg-black dark:bg-opacity-70 bg-white bg-opacity-70 rounded-lg my-1">
-                            <Link href="#project" className={activeSection == 'project' ? 'text-purple-600' : ''}>Project</Link>
-                        </li>
-                        <li className="dark:bg-black dark:bg-opacity-70 bg-white bg-opacity-70 rounded-lg my-1">
-                            <Link href="#contact" className={activeSection == 'contact' ? 'text-purple-600' : ''}>Contact</Link>
-                        </li>
-                    </ul>
-                </div>
+                        <ul className="flex flex-col">
+                            <li className="dark:bg-black dark:bg-opacity-70 bg-white bg-opacity-70 rounded-lg my-1">
+                                <Link href="#home" className={activeSection == 'home' ? 'text-purple-600' : ''}>Home</Link>
+                            </li>
+                            <li className="dark:bg-black dark:bg-opacity-70 bg-white bg-opacity-70 rounded-lg my-1">
+                                <Link href="#about" className={activeSection == 'about' ? 'text-purple-600' : ''}>About</Link>
+                            </li>
+                            <li className="dark:bg-black dark:bg-opacity-70 bg-white bg-opacity-70 rounded-lg my-1">
+                                <Link href="#project" className={activeSection == 'project' ? 'text-purple-600' : ''}>Project</Link>
+                            </li>
+                            <li className="dark:bg-black dark:bg-opacity-70 bg-white bg-opacity-70 rounded-lg my-1">
+                                <Link href="#contact" className={activeSection == 'contact' ? 'text-purple-600' : ''}>Contact</Link>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
                 <div className="items-center justify-center hidden w-full md:flex md:w-auto">
                     <ul className="flex p-3 font-medium rounded-lg space-x-8">
-                        <li>
+                        <li><Brackets isSelected={activeSection == 'home' ? true : false}>
                             <Link href="#home" className={activeSection == 'home' ? 'text-purple-600' : ''}>Home</Link>
-                        </li>
-                        <li>
+                        </Brackets></li>
+                        <li><Brackets isSelected={activeSection == 'about' ? true : false}>
                             <Link href="#about" className={activeSection == 'about' ? 'text-purple-600' : ''}>About</Link>
-                        </li>
-                        <li>
+                        </Brackets></li>
+                        <li><Brackets isSelected={activeSection == 'project' ? true : false}>
                             <Link href="#project" className={activeSection == 'project' ? 'text-purple-600' : ''}>Project</Link>
-                        </li>
-                        <li>
+                        </Brackets></li>
+                        <li><Brackets isSelected={activeSection == 'contact' ? true : false}>
                             <Link href="#contact" className={activeSection == 'contact' ? 'text-purple-600' : ''}>Contact</Link>
-                        </li>
+                        </Brackets></li>
+
                     </ul>
                 </div>
                 <div className="flex">
