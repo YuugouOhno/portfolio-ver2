8b34faf04102e706a5c8f84792aad8ff4cccdb36
YuugouOhno <yuugou.ohno@gmail.com>
2023-04-18 15:54:21 +0900

    #45 小学生の味方の過去登壇資料を追加

--- components/presentationSection/PresentationLists.js
+++ components/presentationSection/PresentationLists.js
@@ -1,17 +1,22 @@
 const PresentationLists = [
     {
+        id: 'mikata',
+        title: '【技育展】無駄開発\n『小学生の味方』2022.9.10',
+        slides_num: 17,
+    },
+    {
         id: 'boidsmodel',
-        title: '【松濤会】ライトニングトーク第3回\n『THREE.JSで最高の魚の群れを作る』',
+        title: '【松濤会】ライトニングトーク第3回\n『THREE.JSで最高の魚の群れを作る』2023.2.25',
         slides_num: 17,
     },
     {
         id: 'namecard',
-        title: '【技育camp】マンスリーハッカソン-vol1\n努力賞受賞『僕の考えた最強の名刺』',
+        title: '【技育camp】マンスリーハッカソン-vol1\n努力賞受賞『僕の考えた最強の名刺』2023.3.26',
         slides_num: 8,
     },
     {
         id: 'talkimage',
-        title: '【技育camp】マンスリーハッカソン-vol2\n努力賞受賞『Talk Image』',
+        title: '【技育camp】マンスリーハッカソン-vol2\n努力賞受賞『Talk Image』2023.4.16',
         slides_num: 8,
     },
     
