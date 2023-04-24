63fb4ccdf0cbdbf57672900395000c41602d5970
YuugouOhno <yuugou.ohno@gmail.com>
2023-04-24 09:45:05 +0900

    デプロイエラーの解消

--- components/aboutSection/AboutMe.js
+++ components/aboutSection/AboutMe.js
@@ -1,7 +1,7 @@
 import Image from "next/legacy/image";
 
 import AnimatedComponent from '@/components/animation/AppearanceContainer';
-// import GitHubCalendar from 'react-github-calendar';
+import GitHubCalendar from 'react-github-calendar';
 import { useTheme } from 'next-themes';
 
 const AboutMe = () => {
@@ -25,10 +25,10 @@ const AboutMe = () => {
                             2024年4月からチームラボという会社でwebエンジニアをします。
                             <br />趣味は旅行、ジャグリング、ベース、ギターです。あと、人と喋るのが好きです。
                         </p>
-                            {/* <GitHubCalendar username="YuugouOhno" colorScheme={theme} theme={{
+                            <GitHubCalendar username="YuugouOhno" colorScheme={theme} theme={{
                                 light: ['#dcdcdc', '#8a2be2'],
                                 dark: ['#696969', '#cc00ff'],
-                            }} hideColorLegend={true} hideMonthLabels={true} /> */}
+                            }} hideColorLegend={true} hideMonthLabels={true} />
                     </div>
 
                 </div>
