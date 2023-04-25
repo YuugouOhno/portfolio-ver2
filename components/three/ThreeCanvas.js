dc04e56b50d9a11e6646d3f078d19761db260ce3
YuugouOhno <yuugou.ohno@gmail.com>
2023-04-25 14:09:56 +0900

    #67 無駄な要素を削除

--- components/three/ThreeCanvas.js
+++ components/three/ThreeCanvas.js
@@ -1,10 +1,8 @@
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 
-
 import Whale from "@/components/three/Whale"
 import Boids from "@/components/three/Boids"
-import Model from "@/components/three/Fish"
 
 const ThreeCanvas = () => {
     const styles = {
@@ -25,20 +23,15 @@ const ThreeCanvas = () => {
         }
     }
 
-    const modelUrl = './models/tuna.glb';
-
     return (
         <div style={styles.canvasArea}>
             <Canvas style={styles.canvas}>
                 <OrbitControls />
-
                 <ambientLight intensity={0.5} />
                 <spotLight position={[0, 10, 0]} angle={1} penumbra={1} />
                 <pointLight position={[0, 10, 0]} />
-
-                {/* <Whale /> */}
+                <Whale />
                 <Boids />
-                {/* <Model url={modelUrl} position={[2, 0, 0]} id={2}/> */}
             </Canvas>
         </div>
     )
