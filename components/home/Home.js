ae7cec317c60cee7157ea0863a550ac66bdd5a04
YuugouOhno <yuugou.ohno@gmail.com>
2023-06-22 14:54:54 +0900

    名前を復活

--- components/home/Home.js
+++ components/home/Home.js
@@ -6,10 +6,10 @@ const Home = () => {
             <section id="home" className="pt-20 h-screen w-screen flex flex-col justify-center items-center z-30 relative">
                 <div className="mt-56">
                     <h1 className="text-8xl font-bold text-center my-4 text-white">
-
+                        Yuugou Ohno
                     </h1>
                     <h2 className="text-5xl font-bold text-center text-white">
-
+                        web engineer
                     </h2>
                 </div>
             </section >
